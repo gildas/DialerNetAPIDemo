1aa06b3764b7d516c36c04cb80c52ff3e2072a93
Gildas Cherruel <gildas.cherruel@inin.com>
2015-11-17 15:04:55 +0900

    Removed unused namespaces

--- iSelectManager/Models/Campaign.cs
+++ iSelectManager/Models/Campaign.cs
@@ -1,10 +1,6 @@
-﻿using ININ.IceLib.Configuration.Dialer;
-using ININ.IceLib.Configuration.Dialer.DataTypes;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Web;
 
 namespace iSelectManager.Models
 {
