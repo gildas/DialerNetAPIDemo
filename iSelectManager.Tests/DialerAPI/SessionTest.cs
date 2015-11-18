f5d322d4f0b09a4c4d23939790bb44d6eee8f74b
Gildas Cherruel <gildas.cherruel@inin.com>
2015-11-18 22:01:09 +0900

    Imported the Unit Tests

---
+++ iSelectManager.Tests/DialerAPI/SessionTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace iSelectManager.Tests.DialerAPI
+{
+    [TestClass]
+    public class SessionTest
+    {
+        [TestMethod]
+        public void CanConnect()
+        {
+        }
+    }
+}
---
+++ iSelectManager.Tests/DialerAPI/SkillTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace iSelectManager.Tests.DialerAPI
+{
+    [TestClass]
+    public class SkillTest
+    {
+        [TestMethod]
+        public void CanFindWhenExists()
+        {
+            Models.Skill skill = Models.Skill.find_or_create("English");
+        }
+    }
+}
