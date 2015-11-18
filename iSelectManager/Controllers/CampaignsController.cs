6383ba06538efd46ada244174e9d885bb67b693c
Gildas Cherruel <gildas.cherruel@inin.com>
2015-11-18 15:18:43 +0900

    Use a CampaignViewModel for the index as well

--- iSelectManager/Controllers/CampaignsController.cs
+++ iSelectManager/Controllers/CampaignsController.cs
@@ -19,7 +19,7 @@ namespace iSelectManager.Controllers
         // GET: Campaigns
         public ActionResult Index()
         {
-            return View(Campaign.find_all(new List<CampaignConfiguration.Property>()));
+            return View(Campaign.find_all().Select(campaign => new CampaignViewModel { id = campaign.id, DisplayName = campaign.DisplayName }));
         }
 
         // GET: Campaigns/Details/5
