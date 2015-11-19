e3a63d2db37715f20e87e84fee56ef3a4d4befe6
Gildas Cherruel <gildas.cherruel@inin.com>
2015-11-20 04:42:22 +0900

    Changed the bootswatch theme

--- iSelectManager/App_Start/BundleConfig.cs
+++ iSelectManager/App_Start/BundleConfig.cs
@@ -20,11 +20,11 @@ namespace iSelectManager
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/superhero.bootstrap.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/superhero.bootstrap.css",
                       "~/Content/site.css"));
         }
     }
